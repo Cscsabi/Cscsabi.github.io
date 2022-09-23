38679bf8371ed129df645114d7e63b8ebdc25459
Csaba Csizmadia <cscsabi2001@gmail.com>
2022-09-23 13:45:40 +0200

    progressing further

--- app.js
+++ app.js
@@ -1,22 +1,42 @@
 const play = () => {
+  const resultText = document.getElementById("result-text");
   const playerScoreText = document.getElementById("player-score");
   const computerScoreText = document.getElementById("computer-score");
   const computerChoiceText = document.getElementById("computer-choice");
-  const resultText = document.getElementById("result-text");
-  const rockButton = document.getElementById("rock");
-  const paperButton = document.getElementById("paper");
-  const scissorsButton = document.getElementById("scissors");
-  const playerChoice = [rockButton, paperButton, scissorsButton];
-  const computerChoice = ["rock", "paper", "scissors"];
+  const computerOptions = [
+    document.getElementById("computer-rock"),
+    document.getElementById("computer-paper"),
+    document.getElementById("computer-scissors"),
+  ];
+
+  let previousComputerChoice = null;
+  let previousPlayerChoice = null;
   let playerScore = 0;
   let computerScore = 0;
 
   const buttonPressed = () => {
+    const rockButton = document.getElementById("rock");
+    const paperButton = document.getElementById("paper");
+    const scissorsButton = document.getElementById("scissors");
+    const playerChoice = [rockButton, paperButton, scissorsButton];
+    const computerChoice = ["rock", "paper", "scissors"];
+
     playerChoice.forEach((player) => {
       player.addEventListener("click", function () {
+        if (previousComputerChoice !== null) {
+          previousComputerChoice.style.backgroundColor = "";
+        }
+        if (previousPlayerChoice !== null) {
+          previousPlayerChoice.style.backgroundColor = "";
+        }
         const computer = computerChoice[Math.floor(Math.random() * 3)];
-        computerChoiceText.textContent = capitalize(computer);
+        computerChoiceText.textContent = `Computer chose: ${capitalize(
+          computer
+        )}`;
         checkWhoWon(this.innerText.toLowerCase(), computer);
+        player.style.backgroundColor = "green";
+        previousComputerChoice = getComputersButton(computer);
+        previousPlayerChoice = player;
       });
     });
   };
@@ -43,6 +63,7 @@ const play = () => {
         playerWon();
       }
     }
+    highlightComputersChoice(computer);
   };
 
   const computerWon = () => {
@@ -62,10 +83,41 @@ const play = () => {
     computerScore = 0;
     playerScoreText.textContent = playerScore;
     computerScoreText.textContent = computerScore;
+    resultText.textContent = "Choose a hand!";
+    computerChoiceText.textContent = "";
+    previousComputerChoice.style.backgroundColor = "";
+    previousPlayerChoice.style.backgroundColor = "";
+    previousComputerChoice = null;
+    previousPlayerChoice = null;
+  };
 
-    // TODO: Picture reset
+  document
+    .getElementById("reset-button")
+    .addEventListener("click", function () {
+      resetGame();
+    });
 
-    play();
+  const highlightComputersChoice = (computer) => {
+    if (computer === "rock") {
+      computerOptions[0].style.backgroundColor = "red";
+      previousComputerChoice = computerOptions[0];
+    } else if (computer === "paper") {
+      computerOptions[1].style.backgroundColor = "red";
+      previousComputerChoice = computerOptions[0];
+    } else {
+      computerOptions[2].style.backgroundColor = "red";
+      previousComputerChoice = computerOptions[0];
+    }
+  };
+
+  const getComputersButton = (computer) => {
+    if (computer === "rock") {
+      return computerOptions[0];
+    } else if (computer === "paper") {
+      return computerOptions[1];
+    } else {
+      return computerOptions[2];
+    }
   };
 
   buttonPressed();
